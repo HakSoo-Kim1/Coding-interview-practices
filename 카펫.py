164d4da526b42e78b7af971e0ddc9e323abe0b3f
HakSooKim <HakSoo.Kim.1@stonybrook.edu>
2021-01-11 19:00:17 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ 카펫.py
@@ -0,0 +1,7 @@
+def solution(brown, yellow):
+    answer = []
+    brown = brown - 4
+    for height in range( 1 , yellow + 1):
+        width = yellow / height
+        if (height * 2) + (width * 2) == brown and width <= height:
+            return [height + 2, width + 2]
