6a4f7c43f164975a560e35079241ecedafe7cff0
HakSooKim <HakSoo.Kim.1@stonybrook.edu>
2021-01-28 21:41:34 +0900

    Merge remote-tracking branch 'origin/master'

--- 섬 연결하기.py
+++ 섬 연결하기.py
@@ -2,7 +2,7 @@ def solution(n, costs):
     lst = [i for i in range(n)]
     costs.sort(key = lambda x: x[2])
     answer = 0
-    print(costs)
+    # print(costs)
     for i in range(len(costs)):
         if (lst[costs[i][0]] != lst[costs[i][1]]):
             answer += costs[i][2]
@@ -11,7 +11,5 @@ def solution(n, costs):
             lst[costs[i][1]] = lst[min(costs[i][0],costs[i][1])]
             for idx in range(len(lst)):
                 if lst[idx] == temp_max:
-                    lst[idx] = min(costs[i][0],costs[i][1])
-        print(lst)
-        print(answer)
+                    lst[idx] = lst[min(costs[i][0],costs[i][1])]
     return answer
